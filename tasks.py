41fbc03c1f1b2d5c8cff1917efc5b07742718235
Josh Peak <neozenith.dev@gmail.com>
2021-11-25 22:33:23 +1100

    add basic qa tasks and config

--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Standard Library
 import os
-import sys
 import shlex
+import sys
 from subprocess import run
 
 # NOTE:
@@ -11,14 +11,28 @@ from subprocess import run
 # 2. invoke <name of task>
 #    - This should successfully import invoke and task decorator
 try:
+    # Third Party
     from invoke import task
 except ImportError:
-    task = lambda x: x
+    task = lambda x: x  # noqa: E731
+
+
+@task
+def format(c):
+    c.run("black .")
+    c.run("isort .")
 
 
 @task
 def lint(c):
-    print("lint task")
+    c.run("black --check .")
+    c.run("isort --check .")
+    c.run("flake8 .")
+
+
+@task
+def test(c):
+    c.run("python3 -m pytest")
 
 
 def _shcmd(command, args=[], **kwargs):
@@ -44,7 +58,7 @@ if __name__ == "__main__":
         if os.path.isfile("requirements-dev.txt"):
             _shcmd(".venv/bin/python3 -m pip install --upgrade -r requirements-dev.txt")
     else:
-        print(f"This script should be run as:\n\n./tasks.py init\n\n")
+        print("This script should be run as:\n\n./tasks.py init\n\n")
         print("This will self bootstrap a virtual environment but then use:\n\n")
         print(". ./.venv/bin/activate")
         print("invoke --list")
